276040da0bb46efdcb479291cf022b4b4e146829
xx10222 <yjw4960@gmail.com>
2023-04-14 19:30:44 +0900

    :art: gitignore 추가

--- src/main/java/org/yammy/boj/boj2501.java
+++ src/main/java/org/yammy/boj/boj2501.java
@@ -1,7 +1,28 @@
 package org.yammy.boj;
 
-public class boj2501 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+public class boj2501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int count = 0; //약수의 개수
+        int answer = 0; //약수
+        for(int i=1;i<=n;i++) {
+            if(n%i==0) {
+                count++;
+            }
+            if(count == k) {
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
     }
 }
